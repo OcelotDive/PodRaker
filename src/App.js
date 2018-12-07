0502d2386598438ee92a45e787d69b0124c7df70
ocelotdive <oscar1dlh@hotmail.com>
2018-12-07 12:06:37 +0000

    p.j

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ class App extends Component {
 	
   render() {
     return (
-		<BrowserRouter>
+		<BrowserRouter  >
       <div className="App">
      	
 		
 		
-		<Route exact path="https://ocelotdive.github.io/podraker/" component={Dashboard}/>
-		<Route exact path="/layout" component={Layout}/>
-		<Route exact path="/player:id" component={Player}/>
+		<Route exact path="#/" component={Dashboard}/>
+		<Route exact path="#/layout" component={Layout}/>
+		<Route exact path="#/player:id" component={Player}/>
       </div>
 		</BrowserRouter>
     );
